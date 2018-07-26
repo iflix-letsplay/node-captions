731e2444b5304531a6c1c6133053f989d90c11f0
Jakub Riedl <email@jakubriedl.cz>
2018-07-26 15:11:15 +1000

    change the way how ttml is produced

--- test/ttml-test.js
+++ test/ttml-test.js
@@ -1,27 +1,24 @@
 var captions = require('../captions.js'),
-    should = require('should');
+    should = require('should')
+    fs = require('fs');
 
 describe('Read SCC file, generate TTML', function () {
     var SCCFile,
         jsonObj,
-        ttmlFile;
+        ttmlFile,
+        sample = fs.readFileSync('./test/captions/test_ttml.xml').toString();
 
     before(function(done) {
-        captions.scc.read('./test/captions/test.scc', {}, function(err, data) {
+        captions.srt.read('./test/captions/test.srt', {}, function(err, data) {
             if (err) { throw 'ERROR Reading test SCC file: ' + err; }
             SCCFile = data;
-            jsonObj = captions.scc.toJSON(data);
+            jsonObj = captions.srt.toJSON(data);
             ttmlFile = captions.ttml.generate(jsonObj);
             done();
         });
     });
 
-    it('should have a length of 109119', function(done) {
-        ttmlFile.length.should.equal(109119);
-        done();
-    });
-
-    it('allow to change header', function(done) {
+    it('return valid ttml', function(done) {
         var customHeader = [
           "<?xml version=\"1.0\" encoding=\"utf-8\"?>",
           "<tt xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.w3.org/ns/ttml\" xmlns:tt=\"http://www.w3.org/ns/ttml\" xmlns:tts=\"http://www.w3.org/ns/ttml#styling\" xmlns:ttp=\"http://www.w3.org/ns/ttml#parameter\" xml:lang=\"en-US\" ttp:timeBase=\"smpte\" ttp:frameRate=\"24\" ttp:frameRateMultiplier=\"1000 1001\" ttp:dropMode=\"nonDrop\">",
@@ -33,8 +30,12 @@ describe('Read SCC file, generate TTML', function () {
           "<body>",
           "<div>"
         ].join('\n')
-        var customHeaderTtmlFile = captions.ttml.generate(jsonObj, { header: [customHeader] });
-        customHeaderTtmlFile.should.include(customHeader)
+        ttmlFile = captions.ttml.generate(jsonObj, {
+          header: [customHeader],
+          lineTemplate: '<p begin="{startTime}" style="normal" end="{endTime}">{text}</p>'
+        });
+        // fs.writeFileSync('./test/captions/test_ttml.xml', ttmlFile) // use this line to update sample
+        ttmlFile.should.equal(sample)
         done();
     });
 });
