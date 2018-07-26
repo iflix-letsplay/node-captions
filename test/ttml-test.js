ffac8c4888050a929433f115fdabf3c9d6b99b41
Jakub Riedl <email@jakubriedl.cz>
2018-07-26 11:45:50 +1000

    Make TTML more configurable

--- test/ttml-test.js
+++ test/ttml-test.js
@@ -16,8 +16,25 @@ describe('Read SCC file, generate TTML', function () {
         });
     });
 
-    it('should have a length of 109297', function(done) {
-        ttmlFile.length.should.equal(109297);
+    it('should have a length of 109119', function(done) {
+        ttmlFile.length.should.equal(109119);
+        done();
+    });
+
+    it('allow to change header', function(done) {
+        var customHeader = [
+          "<?xml version=\"1.0\" encoding=\"utf-8\"?>",
+          "<tt xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.w3.org/ns/ttml\" xmlns:tt=\"http://www.w3.org/ns/ttml\" xmlns:tts=\"http://www.w3.org/ns/ttml#styling\" xmlns:ttp=\"http://www.w3.org/ns/ttml#parameter\" xml:lang=\"en-US\" ttp:timeBase=\"smpte\" ttp:frameRate=\"24\" ttp:frameRateMultiplier=\"1000 1001\" ttp:dropMode=\"nonDrop\">",
+          "<head>",
+          "<styling>",
+          "<style xml:id=\"normal\" tts:color=\"white\" tts:backgroundColor=\"black\" tts:opacity=\"0.70\" tts:fontFamily=\"monospace\" tts:fontSize=\"80%\" tts:fontWeight=\"normal\" tts:fontStyle=\"normal\"/>",
+          "</styling>",
+          "</head>",
+          "<body>",
+          "<div>"
+        ].join('\n')
+        var customHeaderTtmlFile = captions.ttml.generate(jsonObj, { header: [customHeader] });
+        customHeaderTtmlFile.should.include(customHeader)
         done();
     });
 });
